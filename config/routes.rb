cf32c8e71e2dc4e6c6879b49f64f117f9b5fbb89
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-08-13 08:38:56 +0000

    [Update] ルーティングとアソシエーション

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
+  root to: "homes#top"
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :books
+  resources :users, only: [:show :edit]
 end
