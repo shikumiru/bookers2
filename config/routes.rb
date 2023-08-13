34702a87821acafcc56e7d35b194377fa5bfadcc
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-08-13 09:27:33 +0000

    [Add] aboutページ

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :books
-  resources :users, only: [:show :edit]
+  resources :users, only: [:show, :edit, :update]
+
+  get "/homes/about" => "homes#about", as: "about"
 end
