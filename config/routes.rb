b93e180c8c26ca8450f7ae1d5046b985dbd035ea
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-08-14 06:24:05 +0000

    [Add] ヘッダーとそのリンク

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :books
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :index, :edit, :update]
 
   get "/homes/about" => "homes#about", as: "about"
 end
