bfe31b4626d764ee500d36415eb255cc788b0839
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-08-16 09:02:03 +0000

    [Add]Book一覧ページの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,31 @@ class BooksController < ApplicationController
   def new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path
+    else
+      render :index
+    end
+  end
+
   def index
+    @user = current_user
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
