0c6de843b978fd0c2427c96bbe0c01eee9d804b8
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-08-16 14:41:36 +0000

    [Update] Book詳細ページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path
+      redirect_to book_path(@book.id)
     else
       render :index
     end
@@ -19,11 +19,19 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
   private
 
   def book_params
