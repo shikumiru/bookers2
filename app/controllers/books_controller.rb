f1e8369cd3b673e98a77165a27edc0de809f12f6
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-08-16 16:14:02 +0000

    [Add]Book Editページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,6 +24,13 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
@@ -31,7 +38,7 @@ class BooksController < ApplicationController
     book.destroy
     redirect_to books_path
   end
-  
+
   private
 
   def book_params
